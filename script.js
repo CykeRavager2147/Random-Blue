30a9df21443f6a53dff017509298aefdae0c28d2
CykeRavager <oclecouteur@gmail.com>
2023-08-23 22:29:08 -0700

    More Questions

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const questions = [
 
 let elementQuestion = document.getElementById("question");
 console.log(elementQuestion);
-let answerButtons = document.querySelector(".button");
+let answerButtons = document.querySelector(".answerButton");
 let next = document.getElementById("next");
 let currentQuestionIndex = 0;
 let score = 0;
@@ -81,6 +81,8 @@ function startQuiz() {
 }
 
 function showQuestion() {
+  resetState();
+
   let currentQuestion = questions[currentQuestionIndex];
   console.log(currentQuestion);
   let questionNumber = currentQuestionIndex + 1;
@@ -94,24 +96,62 @@ function showQuestion() {
     if (answer.correct) {
       button.dataset.correct = answer.correct;
       console.log(button.dataset.correct);
+      // line 100 !!!
     }
     button.addEventListener("click", selectAnswer);
   });
 }
 
 function selectAnswer(event) {
-  // line 100 !!!
   let selectedButton = event.target;
   console.log(selectedButton);
   let isCorrect = selectedButton.dataset.correct === "true";
   console.log(isCorrect);
-if (isCorrect){
-  selectedButton.classList.add('correct')
+  if (isCorrect) {
+    selectedButton.classList.add("correct");
+  } else {
+    selectedButton.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).map((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = "true";
+  });
+  next.style.display = "block";
+  // Point B - When clicking on buttons actiaully does something (logs the clicked button) - Point C is earlier.
 }
-else{
-  selectedButton.classList.add('incorrect')
+
+function showScore(){
+  resetState()
+  elementQuestion.innerHTML=`You Scored ${score} out of ${questions.length}`
+  next.innerHTML="Play again"
+  next.style.display="block"
+};
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore()
+  }
 }
-  // Point B - When clicking on buttons actiaully does something (logs the clicked button)
+
+next.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+function resetState() {
+  next.style.display = "none";
+
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
 }
 
 startQuiz();
